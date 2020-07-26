bacf9f74e5953bfa73c88dab0a4d2dcf3c3f93dc
Rishita Kar <shreya097@gmail.com>
2020-07-26 16:34:24 -0700

    Implement image sliding

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
- 
+const imagesContainer = document.querySelector('.images-container')
+const imageCount = document.querySelectorAll('.image').length
+let currentImageIndex = 0
+
+document.querySelectorAll('.image').forEach(function (image, index) {
+    image.style.left = (index * 100) + '%'
+})
+
+document.querySelector('.left-arrow-container').addEventListener('click', function () {
+    currentImageIndex--
+    if (currentImageIndex === -1) {
+        currentImageIndex = imageCount - 1
+    }
+    imagesContainer.style.left = (currentImageIndex * -100) + '%'
+})
+
+document.querySelector('.right-arrow-container').addEventListener('click', function () {
+    currentImageIndex++
+    if (currentImageIndex === imageCount) {
+        currentImageIndex = 0
+    }
+    imagesContainer.style.left = (currentImageIndex * -100) + '%'
+})

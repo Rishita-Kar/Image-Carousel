cf293e14ab519256174a59ee3500f610facba63e
Rishita Kar <shreya097@gmail.com>
2020-07-26 18:51:11 -0700

    Add captions to the images

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const imagesContainer = document.querySelector('.images-container')
-const imageCount = document.querySelectorAll('.image').length
+const imageCount = document.querySelectorAll('.image-container').length
 let imageContainerPosition = 0
 let currentImageIndex = 0
 
-document.querySelectorAll('.image').forEach(function (image, index) {
-    image.style.left = (index * 100) + '%'
+document.querySelectorAll('.image-container').forEach(function (imageContainer, index) {
+    imageContainer.style.left = (index * 100) + '%'
 })
 
 document.querySelector('.left-arrow-container').addEventListener('click', function () {
@@ -26,7 +26,7 @@ document.querySelector('.right-arrow-container').addEventListener('click', funct
 })
 
 function slideImages () {
-    const currentImage = imagesContainer.querySelector(`.image:nth-child(${currentImageIndex + 1})`)
-    currentImage.style.left = (imageContainerPosition * -100) + '%'
+    const currentImageContainer = imagesContainer.querySelector(`.image-container:nth-child(${currentImageIndex + 1})`)
+    currentImageContainer.style.left = (imageContainerPosition * -100) + '%'
     imagesContainer.style.left = (imageContainerPosition * 100) + '%'
 }

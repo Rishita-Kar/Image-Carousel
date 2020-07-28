b4bb666441090b3269ba914b0960d9000e8847ea
Rishita Kar <shreya097@gmail.com>
2020-07-28 13:04:31 -0700

    Implement navigation button functionality

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ let currentImageIndex = 0
 document.querySelectorAll('.image-container').forEach(function (imageContainer, index) {
     const navigationCircle = document.createElement('span')
     navigationCircle.className = 'navigation-circle'
+    navigationCircle.addEventListener('click', function() {
+        imageJump(index)
+    })
     navigation.appendChild(navigationCircle)
     if (index === 0) {
         navigationCircle.classList.add('navigation-circle-active')
@@ -34,10 +37,28 @@ document.querySelector('.right-arrow-container').addEventListener('click', funct
 
 function slideImages () {
     const currentImageContainer = imagesContainer.querySelector(`.image-container:nth-child(${currentImageIndex + 1})`)
-    const lastNavigationCircleActive =navigation.querySelector('.navigation-circle-active')
     const currentImageNavigationCircle = navigation.querySelector(`.navigation-circle:nth-child(${currentImageIndex + 1})`)
+    const lastNavigationCircleActive = navigation.querySelector('.navigation-circle-active')
     lastNavigationCircleActive.classList.remove('navigation-circle-active')
     currentImageNavigationCircle.classList.add('navigation-circle-active')
     currentImageContainer.style.left = (imageContainerPosition * -100) + '%'
     imagesContainer.style.left = (imageContainerPosition * 100) + '%'
 }
+
+function imageJump (navigationCircleIndex) {
+    currentImageIndex = navigationCircleIndex
+    if (imageContainerPosition = currentImageIndex) {
+        imageContainerPosition = currentImageIndex
+    }
+
+     else if (imageContainerPosition > currentImageIndex) {
+        imageContainerPosition = currentImageIndex + imageCount
+    }
+
+    else if (imageContainerPosition < currentImageIndex) {
+        imageContainerPosition = currentImageIndex + imageCount
+    }
+
+    slideImages()
+}
+ 
